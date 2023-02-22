fe43bba8b1c44cdd99a2846dbd2168b7cf1b3e6f
Cristopher Namchee <cristopher@tokopedia.com>
2023-02-22 19:49:26 +0700

    fix(cmd): create non-empty cmd file

--- cmd/go-clean-interface/main.go
+++ cmd/go-clean-interface/main.go
@@ -1,5 +1,10 @@
-package noitf
+package gocleaninterface
+
+import (
+	"github.com/Namchee/go-clean-interface/pkg/analyzer"
+	"golang.org/x/tools/go/analysis/singlechecker"
+)
 
 func main() {
-
+	singlechecker.Main(analyzer.Analyzer)
 }
